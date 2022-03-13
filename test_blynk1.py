606ceb1cfc576c55aa5904a492a719fa0e73c4d1
Mike <73032612+Wazowski-09@users.noreply.github.com>
2022-03-13 22:31:49 +0700

    Update test_blynk1.py

--- test_blynk1.py
+++ test_blynk1.py
@@ -8,7 +8,7 @@ blynk = BlynkLib.Blynk('GQNVXrn_3gXzL1F1Ca-efx9Dr_Alklmo')
 @blynk.VIRTUAL_WRITE(1)
 def my_write_handler(value):
     print('Current V1 value: {}'.format(value))
-    blynk.virtual_write(2, format(value))
+    blynk.virtual_write(2, format(value[0]))
 
 #@blynk.VIRTUAL_READ(2)
 #def my_read_handler():
