7d0317feb89fed1f4b9a0b570a77dbcc4006f42b
Mike <73032612+Wazowski-09@users.noreply.github.com>
2022-03-13 21:32:02 +0700

    Create test_blynk1.py

---
+++ test_blynk1.py
@@ -0,0 +1,17 @@
+import BlynkLib
+
+# Initialize Blynk
+blynk = BlynkLib.Blynk('YourAuthToken')
+
+# Register Virtual Pins
+@blynk.VIRTUAL_WRITE(1)
+def my_write_handler(value):
+    print('Current V1 value: {}'.format(value))
+
+@blynk.VIRTUAL_READ(2)
+def my_read_handler():
+    # this widget will show some time in seconds..
+    blynk.virtual_write(2, int(time.time()))
+
+while True:
+    blynk.run()
