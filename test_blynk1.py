13c6bc2922e2f098ca54a5c70d569bc707343ba3
Mike <73032612+Wazowski-09@users.noreply.github.com>
2022-03-13 22:24:58 +0700

    Update test_blynk1.py

--- test_blynk1.py
+++ test_blynk1.py
@@ -4,15 +4,19 @@ import time
 # Initialize Blynk
 blynk = BlynkLib.Blynk('GQNVXrn_3gXzL1F1Ca-efx9Dr_Alklmo')
 
+x = 0
+
 # Register Virtual Pins
 @blynk.VIRTUAL_WRITE(1)
 def my_write_handler(value):
+    global x
+    x = int(format(value))
     print('Current V1 value: {}'.format(value))
 
 @blynk.VIRTUAL_READ(2)
 def my_read_handler():
     # this widget will show some time in seconds..
-    blynk.virtual_write(2, int(time.time()))
+    blynk.virtual_write(2, x)
     
 @blynk.VIRTUAL_WRITE(3)
 def my_write_handler(value):
