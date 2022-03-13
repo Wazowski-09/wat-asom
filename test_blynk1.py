a1f5d20ab0df0e9a2894c7f9b065a562234cef4b
Mike <73032612+Wazowski-09@users.noreply.github.com>
2022-03-13 21:58:09 +0700

    Update test_blynk1.py

--- test_blynk1.py
+++ test_blynk1.py
@@ -12,7 +12,7 @@ def my_write_handler(value):
 @blynk.VIRTUAL_READ(2)
 def my_read_handler():
     # this widget will show some time in seconds..
-    blynk.virtual_write(2, "1")
+    blynk.virtual_write(2, int(time.time()))
 
 while True:
     blynk.run()
