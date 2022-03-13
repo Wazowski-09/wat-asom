0071ff104e15af47cd31f7c9372a8d9e4baabcc4
Mike <73032612+Wazowski-09@users.noreply.github.com>
2022-03-13 22:15:07 +0700

    Update test_blynk1.py

--- test_blynk1.py
+++ test_blynk1.py
@@ -13,6 +13,10 @@ def my_write_handler(value):
 def my_read_handler():
     # this widget will show some time in seconds..
     blynk.virtual_write(2, int(time.time()))
+    
+@blynk.VIRTUAL_WRITE(3)
+def my_write_handler(value):
+    print('Current V3 value: {}'.format(value))
 
 while True:
     blynk.run()
