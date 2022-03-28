0b30c909767585ba4f5c87a6791aaff137903b01
Mike <73032612+Wazowski-09@users.noreply.github.com>
2022-03-28 09:33:35 +0700

    Update test-relay.py

--- test-relay.py
+++ test-relay.py
@@ -4,7 +4,9 @@ GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
  
 RELAIS_1_GPIO = 16
 GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
-GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
-time.sleep(1)
-GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)
-time.sleep(1)
+
+while True:
+    GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
+    time.sleep(1)
+    GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)
+    time.sleep(1)
