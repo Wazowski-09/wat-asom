58ab4357016e1e10b541fe20025a015854eff7d2
Mike <73032612+Wazowski-09@users.noreply.github.com>
2022-03-28 10:52:27 +0700

    Create test-switch.py

---
+++ test-switch.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as GPIO
+from time import sleep
+import time
+GPIO.setmode(GPIO.BCM)
+
+RELAIS_2_GPIO = 16
+RELAIS_3_GPIO = 12
+RELAIS_4_GPIO = 7
+
+inG = 8
+inR = 25
+
+GPIO.setwarnings(False)
+GPIO.setup(RELAIS_2_GPIO, GPIO.OUT) # GPIO Assign mode
+GPIO.setup(RELAIS_3_GPIO, GPIO.OUT)
+GPIO.setup(RELAIS_4_GPIO, GPIO.OUT)
+
+ # set input push button pin
+GPIO.setup(pushpin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # with pull up resistor
+
+while True:
+    button_stateG = GPIO.input(inG)
+    button_stateR = GPIO.input(inR)
+    print(button_stateG)
+    print(button_stateR)
+    if button_stateG == 1 and button_stateR == 0:
+      GPIO.output(RELAIS_2_GPIO, GPIO.LOW) # out
+      GPIO.output(RELAIS_3_GPIO, GPIO.LOW)
+      GPIO.output(RELAIS_4_GPIO, GPIO.LOW)
+    else if button_stateG == 0 and button_stateR == 1:
+      GPIO.output(RELAIS_2_GPIO, GPIO.HIGH) # out
+      GPIO.output(RELAIS_3_GPIO, GPIO.HIGH)
+      GPIO.output(RELAIS_4_GPIO, GPIO.HIGH)
