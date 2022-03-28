f92339be575313ef273ffa340534128c2f65abf7
Mike <73032612+Wazowski-09@users.noreply.github.com>
2022-03-28 15:35:59 +0700

    Update test-s.py

--- test-s.py
+++ test-s.py
@@ -3,10 +3,11 @@ from time import sleep
 GPIO.setmode(GPIO.BCM)
 
 pushpin = 25 # set input push button pin
-#pushpin = 8
+pushpin1 = 8
 
 GPIO.setup(pushpin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # with pull up resistor
+GPIO.setup(pushpin1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    print(GPIO.input(pushpin))
+    print(GPIO.input(pushpin)," ",GPIO.input(pushpin1))
     sleep(0.2)
