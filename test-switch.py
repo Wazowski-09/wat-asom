e26969b575aabdc526b8bf9051b04035f3d2f7af
Mike <73032612+Wazowski-09@users.noreply.github.com>
2022-03-28 11:09:37 +0700

    Update test-switch.py

--- test-switch.py
+++ test-switch.py
@@ -22,13 +22,15 @@ GPIO.setup(inR, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while True:
     button_stateG = GPIO.input(inG)
     button_stateR = GPIO.input(inR)
-    print(button_stateG)
-    print(button_stateR)
+    #print(button_stateG)
+    #print(button_stateR)
     if button_stateG == 1 and button_stateR == 0:
+      print("open")
       GPIO.output(RELAIS_2_GPIO, GPIO.LOW) # out
       GPIO.output(RELAIS_3_GPIO, GPIO.LOW)
       GPIO.output(RELAIS_4_GPIO, GPIO.LOW)
     elif button_stateG == 0 and button_stateR == 1:
+      print("close")
       GPIO.output(RELAIS_2_GPIO, GPIO.HIGH) # out
       GPIO.output(RELAIS_3_GPIO, GPIO.HIGH)
       GPIO.output(RELAIS_4_GPIO, GPIO.HIGH)
