7cea05c8c64c478dc60e560d2e21efe56c5fb06c
Mike <73032612+Wazowski-09@users.noreply.github.com>
2022-03-28 09:37:43 +0700

    Update test-relay.py

--- test-relay.py
+++ test-relay.py
@@ -1,12 +1,40 @@
+#import RPi.GPIO as GPIO
+#import time
+#GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
+ 
+#RELAIS_1_GPIO = 16
+#GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
+
+#while True:
+#    GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
+#    time.sleep(1)
+#    GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)
+#    time.sleep(1)
+
+
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
- 
+
+GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+
 RELAIS_1_GPIO = 16
 GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
 
-while True:
-    GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
-    time.sleep(1)
-    GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)
-    time.sleep(1)
+# Initial state for LEDs:
+print("Testing RF out, Press CTRL+C to exit")
+
+try:
+     print("set GIOP high")
+     GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
+     time.sleep(1)
+     GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)
+     time.sleep(1)             
+except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+   print("Keyboard interrupt")
+
+except:
+   print("some error") 
+
+finally:
+   print("clean up") 
+   GPIO.cleanup() # cleanup all GPIO 
