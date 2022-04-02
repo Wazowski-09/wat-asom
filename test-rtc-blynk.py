b6bd97aeef95da558d52cfb2874d19cd815e2409
Mike <73032612+Wazowski-09@users.noreply.github.com>
2022-04-02 18:19:28 +0700

    Create test-rtc-blynk.py

---
+++ test-rtc-blynk.py
@@ -0,0 +1,25 @@
+import blynklib
+from datetime import datetime
+
+BLYNK_AUTH = 'GQNVXrn_3gXzL1F1Ca-efx9Dr_Alklmo'
+blynk = blynklib.Blynk(BLYNK_AUTH)
+
+
+@blynk.handle_event("connect")
+def connect_handler():
+    blynk.internal("rtc", "sync")
+    print("RTC sync request was sent")
+
+
+@blynk.handle_event('internal_rtc')
+def rtc_handler(rtc_data_list):
+    hr_rtc_value = datetime.utcfromtimestamp(int(rtc_data_list[0])).strftime('%Y-%m-%d %H:%M:%S')
+    print('Raw RTC value from server: {}'.format(rtc_data_list[0]))
+    print('Human readable RTC value: {}'.format(hr_rtc_value))
+
+
+###########################################################
+# infinite loop that waits for event
+###########################################################
+while True:
+    blynk.run()
